0d4a9dd7ff4e40fb7bea3b4e974f09b17b43a116
Emamimari <emamimariam1980@gmail.com>
2023-10-07 19:09:10 +0300

    created task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N (то есть N% 2 = 0)
+Console.Write("Введите число"); 
+int N = Convert.ToInt32(Console.ReadLine());
+Array.Length = int N;
+int Array = [1, N];
+Count = 0;
+while(Count < Array.Lenght)
+{
+    Count++;
+    if(N% 2 = 0)
+    return N;
+}
